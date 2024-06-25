6dab3ea14ad584188b82a2bab34af8bcf1bce1cb
Josiel Castillo <josielcastillov@gmail.com>
2024-06-25 09:10:36 -0600

    Agregar archivos de proyecto.

---
+++ CiclorFor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclorFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables 
+            int sumaEdad = 0;
+            int promedioEdad = 0;
+
+            Console.WriteLine("Ingrese la cantidad de personas: ");
+            int cantidadPersonas = int.Parse(Console.ReadLine());
+
+            
+            for (int i = 0; i < cantidadPersonas; i++)
+            {
+                Console.WriteLine("Ingrese la edad de la persona {0}",i+1);
+                sumaEdad += int.Parse(Console.ReadLine());
+
+                //int edad = int.Parse(Console.ReadLine());
+
+                //sumaEdad = sumaEdad + edad;
+                //sumaEdad += edad;
+            }
+
+            Console.WriteLine("La suma de las edades es: " + sumaEdad);
+            Console.WriteLine("El promedio es: " + sumaEdad/cantidadPersonas);
+
+
+            Console.ReadKey();
+        }
+    }
+}
